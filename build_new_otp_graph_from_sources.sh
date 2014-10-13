2670c06c3ee812f03b7ccab74ca46907ebcf8f0d
Jasper Hartong <jasper@calendar42.com>
2014-10-13 16:06:12 +0200

    removed unnecessary graphproperties argument in building graph script

--- build_new_otp_graph_from_sources.sh
+++ build_new_otp_graph_from_sources.sh
@@ -34,16 +34,17 @@ RAM_GB_DEFAULT="21"
 # INPUT ARGUMENTS
 OTP_JAR="${1:-Undefined}"
 BUILD_DIRECTORY="${2:-Undefined}"
-GRAPH_PROPERTIES_PATH="${3:-Undefined}"
-OSM_PBF_URL="${4:-OSM_PBF_URL_DEFAULT}"
-GTFS_URL="${5:-GTFS_URL_DEFAULT}"
-RAM_GB="${6:-RAM_GB_DEFAULT}"
+OSM_PBF_URL="${3:-OSM_PBF_URL_DEFAULT}"
+GTFS_URL="${4:-GTFS_URL_DEFAULT}"
+RAM_GB="${5:-RAM_GB_DEFAULT}"
+
+# INPUT VALIDATION
+if [[ "${OTP_JAR}" == Undefined ]]; then echo "ERROR: NO OTP JAR PASS ALONG"; exit; fi
+if [[ "${BUILD_DIRECTORY}" == Undefined ]]; then echo "ERROR: NO BUILD DIRECTORY PASS ALONG"; exit; fi
 
 # COMPUTED VARIABLES
 OTP_JAR_ABS="${__CURRENT_WORKING_DIR__}/${OTP_JAR}"
 BUILD_DIRECTORY_ABS="${__CURRENT_WORKING_DIR__}/${BUILD_DIRECTORY}"
-GRAPH_PROPERTIES_PATH_ABS="${__CURRENT_WORKING_DIR__}/${GRAPH_PROPERTIES_PATH}"
-GRAPH_PROPERTIES_CONTENT=$(<${GRAPH_PROPERTIES_PATH_ABS})
 SOURCES_JSON="{\"osm.pbf\":\""${OSM_PBF_URL}"\", \"gtfs.zip\":\""${GTFS_URL}"\"}"
 
 echo
@@ -54,16 +55,13 @@ echo "1 CREATING BUILD DIRECTORY IF NECESSARY"
 mkdir -p ${BUILD_DIRECTORY_ABS}
 cd ${BUILD_DIRECTORY_ABS}
 
-echo "2 CREATING GRAPH PROPERTIES FILE"
-echo "${GRAPH_PROPERTIES_CONTENT}" >> Graph.properties
-
-echo "3 DOWNLOADING OPENSTREETMAP DATA"
+echo "2 DOWNLOADING OPENSTREETMAP DATA"
 wget ${OSM_PBF_URL} -O osm.pbf --tries=1 --timeout=600
 
-echo "4 DOWNLOADING GTFS DATA"
+echo "3 DOWNLOADING GTFS DATA"
 wget ${GTFS_URL} -O gtfs.zip --tries=1 --timeout=600
 
-echo "5 BACKING UP LAST BUILD GRAPH AND SOURCES JSON"
+echo "4 BACKING UP LAST BUILD GRAPH AND SOURCES JSON"
 if [ -f Graph.obj ]
 then
     mv Graph.obj Graph.obj.last
@@ -73,13 +71,13 @@ then
     mv lastBuildSources.json lastBuildSources.json.last
 fi
 
-echo "6 SAVE CURRENT BUILD SOURCES"
+echo "5 SAVE CURRENT BUILD SOURCES"
 echo ${SOURCES_JSON} >> lastBuildSources.json
 
-echo "7 BUILD GRAPH"
+echo "6 BUILD GRAPH"
 time java -server -Xmx${RAM_GB}G -jar ${OTP_JAR_ABS} --skipVisibility --longDistance --build .
 
-echo "8 GO BACK TO WHERE WE CAME FROM"
+echo "7 GO BACK TO WHERE WE CAME FROM"
 cd ${__DIR__}
 
 exit
@@ -90,3 +88,4 @@ exit
 
 # USE TIMESTAMPS TO ONLY DOWNLOAD IF NECESSARY
 # Checking for existence of properties
+# Use named variables

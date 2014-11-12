00b37205a0dcf8a11e3a334918010a09c225c702
Jasper Hartong <jasper@calendar42.com>
2014-11-12 09:26:46 +0100

    simplified script

--- runGraphTester.sh
+++ runGraphTester.sh
@@ -12,6 +12,7 @@ set -o errexit
 __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
+
 CURRENT_WORKING_DIR=$(pwd)
 
 # Exit when using undeclared variables
@@ -31,15 +32,22 @@ command_exists () {
 
 # INPUT ARGUMENTS
 GRAPHTESTER_DIRECTORY="${1:-Undefined}"
+LOGGING_DIRECTORY="${2:-Undefined}"
 
 GRAPHTESTER_DIRECTORY=$(cd "$GRAPHTESTER_DIRECTORY" && pwd)
+LOGGING_DIRECTORY=$(cd "$LOGGING_DIRECTORY" && pwd)
+
+
+# INPUT VALIDATION
+if [[ "${GRAPHTESTER_DIRECTORY}" == Undefined ]]; then echo "ERROR: NO GRAPHTESTER_DIRECTORY PASS ALONG"; exit; fi
+if [[ "${LOGGING_DIRECTORY}" == Undefined ]]; then echo "ERROR: NO LOGGING_DIRECTORY PASS ALONG"; exit; fi
 
 
 # START ACTUAL SCRIPT
 echo; echo "1 RUNNING THE TESTS"; echo;
 cd ${GRAPHTESTER_DIRECTORY}
 source graphTesterVirtualEnv/bin/activate
-python2 mmri/test_otp.py tests/static-tests.json ../logging/`date +%Y-%m-%d.%H:%M:%S`.json -u http://0.0.0.0:9050/otp/routers/default/plan --today
+python2 mmri/test_otp.py tests/static-tests.json "${LOGGING_DIRECTORY}/`date +%Y-%m-%d.%H:%M:%S`.json" -u http://0.0.0.0:9050/otp/routers/default/plan --today
 deactivate
 
 cd ${CURRENT_WORKING_DIR}
